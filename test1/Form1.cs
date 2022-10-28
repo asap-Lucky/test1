6a663a963321546cb90c2f2222052587d5cf2ab5
Djoan Golovko <djgo49478@edu.ucl.dk>
2022-10-28 11:09:10 +0200

    :)

--- test1/Form1.cs
+++ test1/Form1.cs
@@ -19,7 +19,7 @@ namespace test1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola amigoooooooooooos");
+            MessageBox.Show("Hola amigoooooooooooosaqulla");
         }
     }
 }
